0cdb66b8bfae5b2a01df9bcbb1c5fe2a0639e23c
sofrone7 <79331990+sofrone7@users.noreply.github.com>
2021-02-19 20:15:49 +0100

    Add files via upload

---
+++ Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bt_Dibujar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(23, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 415);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Resize += new System.EventHandler(this.pictureBox1_Resize);
+            // 
+            // bt_Dibujar
+            // 
+            this.bt_Dibujar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bt_Dibujar.Location = new System.Drawing.Point(576, 156);
+            this.bt_Dibujar.Name = "bt_Dibujar";
+            this.bt_Dibujar.Size = new System.Drawing.Size(111, 38);
+            this.bt_Dibujar.TabIndex = 1;
+            this.bt_Dibujar.Text = "Pintar";
+            this.bt_Dibujar.UseVisualStyleBackColor = true;
+            this.bt_Dibujar.Click += new System.EventHandler(this.bt_Dibujar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 450);
+            this.Controls.Add(this.bt_Dibujar);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button bt_Dibujar;
+    }
+}
+
---
+++ Ejercicio2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        private Point m_Centro = new Point();
+        int m_Radio;
+        public Form1()
+        {
+            InitializeComponent();
+            ActualizarDimensiones();
+        }
+
+        private void ActualizarDimensiones()
+        {
+            m_Centro.X = this.ClientSize.Width / 2;
+            m_Centro.Y = this.ClientSize.Height / 2;
+            m_Radio = Math.Min(m_Centro.X, m_Centro.Y);
+        }
+
+        private void bt_Dibujar_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            Pen lápizNegro = new Pen(Color.Black, 3);
+            Rectangle rect = new Rectangle(100, 100, 100, 100);
+            g.DrawRectangle(lápizNegro, rect);
+
+            Pen lápizAzul = new Pen(Color.Blue, 3);
+            Point[] points2 = { new Point(100, 100), new Point(200, 100), new Point(150, 10) };
+            g.DrawPolygon(lápizAzul, points2);
+
+            Pen lápizAmarillo = new Pen(Color.Yellow, 3);
+            g.DrawEllipse(lápizAmarillo, 250, 5, 100, 100);
+            g.FillEllipse(new SolidBrush(Color.Yellow), 250, 5, 100, 100);
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            ActualizarDimensiones();
+            Invalidate();
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
