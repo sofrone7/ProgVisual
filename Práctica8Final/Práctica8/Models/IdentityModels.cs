da7d4e56e5efecae31bdbcdf6ffbb4a4f9ace91d
Silviu Sofrone <79331990+sofrone7@users.noreply.github.com>
2021-04-07 09:47:17 +0200

    Add files via upload

---
+++ Práctica8Final/Práctica8/Models/IdentityModels.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Práctica8.Models;
+
+namespace Práctica8.Models
+{
+    public class UserManager : UserManager<ApplicationUser>
+    {
+        public UserManager()
+            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+        }
+    }
+    public class ApplicationUser : IdentityUser
+    {
+        // ...
+        public ClaimsIdentity GenerateUserIdentity(ApplicationUserManager manager)
+        {
+            // Tenga en cuenta que authenticationType debe coincidir con el valor definido en CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = manager.CreateIdentity(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Agregar reclamaciones de usuario personalizadas aquí
+            return userIdentity;
+        }
+
+        public Task<ClaimsIdentity> GenerateUserIdentityAsync(ApplicationUserManager manager)
+        {
+            return Task.FromResult(GenerateUserIdentity(manager));
+        }
+        public static ApplicationUser CreateUser(string user_name, string password)
+        {
+            var um = new UserManager();
+            var user = new ApplicationUser() { UserName = user_name };
+            var idResult = um.Create(user, password);
+            if (idResult.Succeeded)
+                return user;
+            return null;
+        }
+
+        public bool AddUserToRole(ApplicationRole role)
+        {
+            var um = new UserManager();
+            var idResult = um.AddToRole(this.Id, role.Name);
+            return idResult.Succeeded;
+        }
+    }
+    // Para agregar datos del usuario, agregue más propiedades a su clase de usuario. Visite https://go.microsoft.com/fwlink/?LinkID=317594 para obtener más información.
+    /*public class ApplicationUser : IdentityUser
+    {
+        
+    }*/
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole() : base() { }
+
+        public ApplicationRole(string name) : base(name) { }
+
+        public static ApplicationRole CreateRole(string name)
+        {
+            var rm = new RoleManager();
+            ApplicationRole role = new ApplicationRole(name);
+            var idResult = rm.Create(role);
+            if (idResult.Succeeded)
+                return role;
+            return null;
+        }
+
+        public static bool FindRole(string name)
+        {
+            var rm = new RoleManager();
+            return rm.RoleExists(name);
+        }
+    }
+    public class RoleManager : RoleManager<ApplicationRole>
+    {
+        public RoleManager()
+            : base(new RoleStore<ApplicationRole>(new ApplicationDbContext()))
+        {
+            System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
+            // Para borrar la base de datos cambiar DropCreateDatabaseIfModelChanges por DropCreateDatabaseAlways
+        }
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
+
+#region Aplicaciones auxiliares
+namespace Práctica8
+{
+    public static class IdentityHelper
+    {
+        // Se utilizan para XSRF al vincular inicios de sesión externos
+        public const string XsrfKey = "XsrfId";
+
+        public const string ProviderNameKey = "providerName";
+        public static string GetProviderNameFromRequest(HttpRequest request)
+        {
+            return request.QueryString[ProviderNameKey];
+        }
+
+        public const string CodeKey = "code";
+        public static string GetCodeFromRequest(HttpRequest request)
+        {
+            return request.QueryString[CodeKey];
+        }
+
+        public const string UserIdKey = "userId";
+        public static string GetUserIdFromRequest(HttpRequest request)
+        {
+            return HttpUtility.UrlDecode(request.QueryString[UserIdKey]);
+        }
+
+        public static string GetResetPasswordRedirectUrl(string code, HttpRequest request)
+        {
+            var absoluteUri = "/Account/ResetPassword?" + CodeKey + "=" + HttpUtility.UrlEncode(code);
+            return new Uri(request.Url, absoluteUri).AbsoluteUri.ToString();
+        }
+
+        public static string GetUserConfirmationRedirectUrl(string code, string userId, HttpRequest request)
+        {
+            var absoluteUri = "/Account/Confirm?" + CodeKey + "=" + HttpUtility.UrlEncode(code) + "&" + UserIdKey + "=" + HttpUtility.UrlEncode(userId);
+            return new Uri(request.Url, absoluteUri).AbsoluteUri.ToString();
+        }
+
+        private static bool IsLocalUrl(string url)
+        {
+            return !string.IsNullOrEmpty(url) && ((url[0] == '/' && (url.Length == 1 || (url[1] != '/' && url[1] != '\\'))) || (url.Length > 1 && url[0] == '~' && url[1] == '/'));
+        }
+
+        public static void RedirectToReturnUrl(string returnUrl, HttpResponse response)
+        {
+            if (!String.IsNullOrEmpty(returnUrl) && IsLocalUrl(returnUrl))
+            {
+                response.Redirect(returnUrl);
+            }
+            else
+            {
+                response.Redirect("~/");
+            }
+        }
+    }
+}
+#endregion
