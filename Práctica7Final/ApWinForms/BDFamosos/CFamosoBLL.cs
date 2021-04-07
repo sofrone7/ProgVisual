c8f718e241bee5b4c7ece415ebae3dae52998fe3
Silviu Sofrone <79331990+sofrone7@users.noreply.github.com>
2021-04-07 09:29:19 +0200

    Add files via upload

---
+++ Práctica7Final/ApWinForms/BDFamosos/CFamosoBLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDFamosos
+{
+    public class CFamosoBLL
+    {
+        private CFamosoDAL bd = new CFamosoDAL();
+        public ColCFamosos ObtenerFilasFamosos()
+        {
+            ColCFamosos coTfnos = bd.ObtenerFilasFamosos();
+            return coTfnos;
+        }
+        public void ActualizarFamosos(CFamosoBO bo)
+        {
+            bd.ActualizarFamosos(bo);
+        }
+    }
+
+}
