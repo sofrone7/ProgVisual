dc8008bb6dfbb63b30bcc2699a5fdff69a645e25
Silviu Sofrone <79331990+sofrone7@users.noreply.github.com>
2021-04-07 09:38:26 +0200

    Add files via upload

---
+++ Práctica8Final/Práctica8/añadir/añadir.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="Añadir foto" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="añadir.aspx.cs" Inherits="Práctica8.añadir" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h2><%: Title %> - Albúm de fotos.</h2>
+        <p>
+            Haga click en "Examinar" para seleccionar una foto. Introduzca el nombre de la foto, el autor
+            y la descripción de la misma. Finalmente haga click en "Añadir".
+        </p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+
+            <asp:Label ID="Label1" runat="server" Text="Selecciona una foto:"></asp:Label>
+            <asp:Label ID="et_Archivo" runat="server" ForeColor="Red"></asp:Label>
+        <asp:FileUpload ID="sa_SubirArchivo" runat="server" />
+            <asp:Label ID="Label4" runat="server" Text="Nombre de la foto:"></asp:Label>
+            <br />
+            <asp:TextBox ID="ct_Nombre" runat="server" OnTextChanged="TextBox1_TextChanged" MaxLength="30"></asp:TextBox>
+            <asp:Label ID="et_Nombre" runat="server" ForeColor="Red"></asp:Label>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Autor:"></asp:Label>
+            <br />
+            <asp:TextBox ID="ct_Autor" runat="server" MaxLength="30"></asp:TextBox>
+            <asp:Label ID="et_Autor" runat="server" ForeColor="Red"></asp:Label>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Descripción:"></asp:Label>
+            <br />
+            <asp:TextBox ID="ct_Descripcion" runat="server" Height="69px" Width="269px" MaxLength="400"></asp:TextBox>
+            <br />
+            <asp:Button ID="bt_Añadir" runat="server" Text="Añadir" OnClick="bt_Añadir_Click" />
+
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Práctica8Final/Práctica8/añadir/añadir.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Práctica8
+{
+    public partial class añadir : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void bt_Añadir_Click(object sender, EventArgs e)
+        {
+            if (FormularioCorrecto)
+            {
+                // La carpeta raíz del sitio web es "~/"
+                string sRutaSitioWeb = Server.MapPath("~/"); // ver documentación de Page.Server
+                                                             // Subimos los archivos al servidor
+                sa_SubirArchivo.PostedFile.SaveAs(sRutaSitioWeb +
+                "fotos/" + sa_SubirArchivo.FileName);
+
+                //Guardamos la información en la base de datos
+                string foto = "fotos/" + sa_SubirArchivo.FileName;
+                string nombre = ct_Nombre.Text;
+                string autor = ct_Autor.Text;
+
+                string descripcion = ct_Descripcion.Text;
+                if (descripcion.Length == 0)
+                    descripcion = "-- Sin Descripción --";
+                /*string sconexión = ConfigurationManager.ConnectionStrings["fotosConnectionString"].ConnectionString; // ver clase ConfigurationManager
+                SqlConnection conexión = new SqlConnection(sconexión);
+                string sOrdenSQL = "insert into album (foto, nombre, autor," +
+                " descripcion) values (@FOTO,@NOMBRE,@AUTOR,@DESCRIPCION)";
+                SqlCommand ordenSQL = new SqlCommand(sOrdenSQL, conexión);
+                ordenSQL.Parameters.AddWithValue("@FOTO", foto);
+                ordenSQL.Parameters.AddWithValue("@NOMBRE", nombre);
+                ordenSQL.Parameters.AddWithValue("@AUTOR", autor);
+                ordenSQL.Parameters.AddWithValue("@DESCRIPCION", descripcion);
+                // Idem para autor, descripcion...
+                // ...
+                ordenSQL.Connection.Open();
+                ordenSQL.ExecuteNonQuery();
+                ordenSQL.Connection.Close();*/
+
+                AlbumFotos.Service1Client servicio = new AlbumFotos.Service1Client();
+                servicio.AñadirFoto(foto, nombre, autor, descripcion);
+
+                this.Response.Redirect("../Default.aspx");
+            }
+        }
+
+        protected bool FormularioCorrecto
+        {
+            get
+            {
+                bool bCorrecto = true;
+                // Volvemos a dejar en blanco los comentarios
+                et_Nombre.Text = "";
+                et_Autor.Text = "";
+                et_Archivo.Text = "";
+                // Comprobamos el formulario
+                if (sa_SubirArchivo.FileName.Length == 0)
+                {
+                    et_Archivo.Text = "* Error: Seleccione un archivo!!";
+                    bCorrecto = false;
+                }
+                // Comprobar también el Nombre y el Autor
+                // ...
+                if(ct_Nombre.Text.Length == 0)
+                {
+                    et_Nombre.Text = "* Error: Introduzca un nombre!";
+                    bCorrecto = false;
+                }
+                if (ct_Autor.Text.Length == 0)
+                {
+                    et_Autor.Text = "* Error: Introduzca un autor!";
+                    bCorrecto = false;
+                }
+                return bCorrecto;
+            }
+        }
+    }
+}
